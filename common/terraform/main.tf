4715a994b7cbcc3c3b97524f07a0c705ea0f2b11
Adam Lewis <dotdashnotdotsoftware@gmail.com>
2023-05-01 12:25:01 +0000

    Capture existing read API Gateway infra as code

--- common/terraform/main.tf
+++ common/terraform/main.tf
@@ -3,6 +3,10 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "The API for the app"
 }
 
+module "readLegacy" {
+  source = "./readLegacy"
+}
+
 resource "aws_api_gateway_method" "post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_rest_api.api.root_resource_id
